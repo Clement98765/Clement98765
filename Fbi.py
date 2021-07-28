a8f169082a4ba99c359414a0f866736fec5fadcf
Clement98765 <74838773+Clement98765@users.noreply.github.com>
2021-07-28 23:39:31 +0100

    Fbi.py
    Facebook phishing link

---
+++ Fbi.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Clement98765
+- 👀 I’m interested in hacking
+- 🌱 I’m currently learning hacking
+- 💞️ I’m looking to collaborate on cyber security
+- 📫 How to reach me wa.me/2348114217090
+
+<!---
+Clement98765/Clement98765 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
